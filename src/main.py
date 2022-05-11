8fe1aefc40c131a25a823e36ad885b99f7968911
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-11 12:07:51 -0500

    main
    Co-authored-by: Elkin Santis <ErSantis@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import os
 from schedule import Calendar
 from peter_assistant import PeterAssistant
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 # from event import Event
 
 asistente = PeterAssistant()
-asistente.greet()
+# asistente.greet()
 calendario = Calendar()
 # text = asistente.listen()
 # print(text)
@@ -13,22 +15,36 @@ calendario = Calendar()
 if True:
     calendario.startServices()
     if calendario.is_available():
-       events = calendario.get_event(
+        events = calendario.get_event(
             calendario.today, calendario.service
         )
     else:
         print("Lo siento, no he podido obtener tus eventos")
 
-  
+
 else:
-    
+
     print("Disculpa no te he entendido, por favor repite")
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def index():
     return jsonify(events)
+
+
+@app.route('/location', methods=["POST"])
+def get_location():
+    print("haciendo post")
+    print(request.json)
+
+    with open('src/data/location.json', 'w') as location:
+        location.write(str(request.json).replace("'", '"'))
+
+    return jsonify({"response": "200"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
