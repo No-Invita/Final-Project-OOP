fc0e75bb69ce9131299326fcf9e2c59b7d100836
Elkin <ersantis@uninorte.edu.co>
2022-05-11 14:08:32 -0500

    Nothing important

--- src/app/peter_assistant.py
+++ src/app/peter_assistant.py
@@ -5,7 +5,6 @@ class PeterAssistant:
     
     voice_id = 'spanish-latin-am'
     listener = sr.Recognizer()
-    
     engine = pyttsx3.init()
     voices = engine.getProperty('voices') # getting to know current volume level (min=0 and max=1)
     engine.setProperty('voice', voices[1].id)
@@ -23,15 +22,18 @@ class PeterAssistant:
         try:
             with sr.Microphone() as source:
                 print("Speak now")
-                voice = self.listener.listen(source)
                 self.listener.energy_threshold = 4000
-                self.listener.adjust_for_ambient_noise(source, duration = 1)
-                command = self.listener.recognize_google(
-                    voice, language='es-CO')
-                command = command.lower()
-                return command
+                self.listener.adjust_for_ambient_noise(source, duration = 0.2)
+                voice = self.listener.listen(source)
+                try:
+                    command = self.listener.recognize_google(
+                        voice, language='es-CO')
+                    command = command.lower()
+                    return command
+                except:
+                    return "Sin conexion, por favor conectate a internet"
         except:
-            return "Sin conexion, por favor conectate a internet"
+            return "Asegurate que tu microfono esté en buen estado"
     
     def speak(self, speech):
         
