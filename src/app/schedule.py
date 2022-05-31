10bcae0d89189b0782191b2676dd2b710acbc1cc
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-05-31 18:31:11 -0500

    validate events information

--- src/app/schedule.py
+++ src/app/schedule.py
@@ -21,14 +21,14 @@ class Calendar(Services):
             print('No tienes eventos por ahora')
         else:
             for event in events:
-                self.start = str(event['start'].get(
-                    'dateTime', event['start'].get('date')).split('T')[1].split("-")[0])
-                self.end = str(event['end'].get('dateTime', event['end'].get(
-                    'date')).split('T')[1].split("-")[0])
-                end_time = datetime.datetime.strptime(self.end, '%X').time()
-                clase = Event(self.end, self.start, str(event['summary']), str(
-                    event['location']), str(event['description']))
+                if (event['start'] and event['end'] and event['summary'] and event['description']):
+                    self.start = str(event['start'].get(
+                        'dateTime', event['start'].get('date')).split('T')[1].split("-")[0])
+                    self.end = str(event['end'].get('dateTime', event['end'].get(
+                        'date')).split('T')[1].split("-")[0])
+                    clase = Event(self.end, self.start, str(event['summary']), str(
+                        event['location']), str(event['description']))
 
-                clases.append(clase.get_clase())
+                    clases.append(clase.get_clase())
 
         return clases
